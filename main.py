32f71d05153d8e620e5689864db5e8be1669518c
TieuLenh <hieukaito2k5@gmail.com>
2025-06-17 19:46:39 +0700

    no comment

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import package.subpack2.module2 as md2
 pl1 = md2.player()
 pl2 = md2.player(1,1)
 myProfile = md2.person(myName, myYob, myHometown)
+
+print(pl1.x, pl1.y)
+print(pl2.x, pl2.y)
+print(myProfile.name, myProfile.yob, myProfile.ht)
--- package/subpack2/module2.py
+++ package/subpack2/module2.py
@@ -1,11 +1,12 @@
 class person:
-  def __init__(self,name,yob,ht):
+  def __init__(self,name = 'default', yob = 0, ht =  'default'):
     self.name = name
     self.yob = yob
     self.ht = ht
+  
 
 
 class player:
-  def __init__(self, x, y):
+  def __init__(self, x = 0, y = 0):
     self.x = x
     self.y = y
