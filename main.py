df25f5d7ff69d0fe9dedc830922f9a15ce72c081
TieuLenh <hieukaito2k5@gmail.com>
2025-06-17 19:36:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from package.subpack1.module1 import *
 import package.subpack2.module2 as md2 
 
-pl = md2.player()
+pl1 = md2.player()
+pl2 = md2.player(1,1)
 myProfile = md2.person(myName, myYob, myHometown)
